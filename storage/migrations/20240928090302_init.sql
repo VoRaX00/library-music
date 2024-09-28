8cb292356c495c1f5d7b0929404466b8c8e0bad3
nikita <nikitakerzhik@gmail.com>
2024-09-28 19:17:27 +1000

    edit config and added migrations

---
+++ storage/migrations/20240928090302_init.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE music (
+    id SERIAL PRIMARY KEY,
+    music_group TEXT NOT NULL,
+    song TEXT NOT NULL,
+    text_song TEXT NOT NULL,
+    link TEXT
+);
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE music;
+-- +goose StatementEnd
