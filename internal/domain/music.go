c1eb7bd4b25fde96b34e80bf76ae65b8f7e153ad
nikita <nikitakerzhik@gmail.com>
2024-10-02 17:01:11 +1000

    refactoring

--- internal/domain/music.go
+++ internal/domain/music.go
@@ -8,32 +8,3 @@ type Music struct {
 	Link        string `json:"link" db:"link"`
 	ReleaseDate string `json:"releaseDate" db:"release_date"`
 }
-
-type MusicToAdd struct {
-	Song        string `json:"song" db:"song"`
-	Group       string `json:"group" db:"music_group"`
-	Text        string `json:"text" db:"text_song"`
-	Link        string `json:"link" db:"link"`
-	ReleaseDate string `json:"releaseDate" db:"release_date"`
-}
-
-type MusicToUpdate struct {
-	Song        string `json:"song" db:"song"`
-	Group       string `json:"group" db:"music_group"`
-	Text        string `json:"text" db:"text_song"`
-	Link        string `json:"link" db:"link"`
-	ReleaseDate string `json:"releaseDate" db:"release_date"`
-}
-
-type MusicToDelete struct {
-	Song  string `json:"song" db:"song"`
-	Group string `json:"group" db:"music_group"`
-}
-
-type MusicToGet struct {
-	Id          int    `json:"id" db:"id"`
-	Song        string `json:"song" db:"song"`
-	Group       string `json:"group" db:"music_group"`
-	Link        string `json:"link" db:"link"`
-	ReleaseDate string `json:"releaseDate" db:"release_date"`
-}
--- internal/domain/requestModels.go
+++ internal/domain/requestModels.go
@@ -2,6 +2,35 @@ package domain
 
 import "time"
 
+type MusicToAdd struct {
+	Song        string `json:"song" db:"song"`
+	Group       string `json:"group" db:"music_group"`
+	Text        string `json:"text" db:"text_song"`
+	Link        string `json:"link" db:"link"`
+	ReleaseDate string `json:"releaseDate" db:"release_date"`
+}
+
+type MusicToUpdate struct {
+	Song        string `json:"song" db:"song"`
+	Group       string `json:"group" db:"music_group"`
+	Text        string `json:"text" db:"text_song"`
+	Link        string `json:"link" db:"link"`
+	ReleaseDate string `json:"releaseDate" db:"release_date"`
+}
+
+type MusicToDelete struct {
+	Song  string `json:"song" db:"song"`
+	Group string `json:"group" db:"music_group"`
+}
+
+type MusicToGet struct {
+	Id          int    `json:"id" db:"id"`
+	Song        string `json:"song" db:"song"`
+	Group       string `json:"group" db:"music_group"`
+	Link        string `json:"link" db:"link"`
+	ReleaseDate string `json:"releaseDate" db:"release_date"`
+}
+
 type MusicFilterParams struct {
 	Song        string    `json:"song"`
 	Group       string    `json:"group"`
