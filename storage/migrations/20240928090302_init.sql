ca3e228b10b81503bb5d19ff5d1ec405c0acb7a5
nikita <nikitakerzhik@gmail.com>
2024-11-06 09:05:13 +1000

    Edit db

--- storage/migrations/20240928090302_init.sql
+++ storage/migrations/20240928090302_init.sql
@@ -2,12 +2,22 @@
 -- +goose StatementBegin
 CREATE TABLE music (
     id SERIAL PRIMARY KEY,
-    music_group TEXT NOT NULL,
     song TEXT NOT NULL,
     text_song TEXT NOT NULL,
     release_date DATE NOT NULL,
     link TEXT,
     CONSTRAINT unique_song_group UNIQUE (song, music_group)
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE music_groups (
+    music_id INTEGER REFERENCES music(id) ON DELETE CASCADE,
+    group_id INTEGER REFERENCES groups(id) ON DELETE CASCADE,
+    PRIMARY KEY (music_id, group_id)
 );
 -- +goose StatementEnd
 -- +goose Down
